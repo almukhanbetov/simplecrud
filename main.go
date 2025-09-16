209a38fca81ab4dd80ce623e9eb08fb0e68d44e5
almukhanbetov <almuko.m@gmail.com>
2025-09-16 09:15:42 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	db := InitDB()
+	defer db.Close()
+
+	r := gin.Default()
+
+	r.GET("/posts", func(c *gin.Context) { GetPosts(c, db) })
+	r.POST("/posts", func(c *gin.Context) { CreatePost(c, db) })
+	r.PUT("/posts/:id", func(c *gin.Context) { UpdatePost(c, db) })
+	r.DELETE("/posts/:id", func(c *gin.Context) { DeletePost(c, db) })
+
+	log.Println("Server started on :8080")
+	r.Run(":8080")
+}
